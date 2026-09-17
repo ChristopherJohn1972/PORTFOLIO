02a1deb8de4c7589c9d5f3af0b123d31980e5680
ChristopherJohn1972 <curlsjamin@gmail.com>
2026-09-17 11:39:42 +0300

    Add EMAIL_TIMEOUT=5s to prevent worker timeout on Render

--- portfolio-api/portfolio/settings.py
+++ portfolio-api/portfolio/settings.py
@@ -197,4 +197,5 @@ EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+EMAIL_TIMEOUT = 5
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'Portfolio <noreply@localhost>')
